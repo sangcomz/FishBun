cc3da8f291c9fd38ecf21e9710677b740de7fe87
laco-dev <laco2951@gmail.com>
2019-09-26 17:20:02 +0900

    Convert interfaces from java to Kotlin "BaseProperty", "CustomizationProperty"

---
+++ FishBun/src/main/java/com/sangcomz/fishbun/CustomizationProperty.kt
@@ -0,0 +1,60 @@
+package com.sangcomz.fishbun
+
+import android.graphics.drawable.Drawable
+
+/**
+ * Created by sangcomz on 13/05/2017.
+ */
+interface CustomizationProperty {
+    fun setAlbumThumbnailSize(size: Int): FishBunCreator
+
+    fun setPickerSpanCount(spanCount: Int): FishBunCreator
+
+    fun setActionBarColor(actionbarColor: Int): FishBunCreator
+
+    fun setActionBarTitleColor(actionbarTitleColor: Int): FishBunCreator
+
+    fun setActionBarColor(actionbarColor: Int, statusBarColor: Int): FishBunCreator
+
+    fun setActionBarColor(actionbarColor: Int, statusBarColor: Int, isStatusBarLight: Boolean): FishBunCreator
+
+    fun setCamera(isCamera: Boolean): FishBunCreator
+
+    fun textOnNothingSelected(message: String?): FishBunCreator
+
+    fun textOnImagesSelectionLimitReached(message: String?): FishBunCreator
+
+    fun setButtonInAlbumActivity(isButton: Boolean): FishBunCreator
+
+    fun setAlbumSpanCount(portraitSpanCount: Int, landscapeSpanCount: Int): FishBunCreator
+
+    fun setAlbumSpanCountOnlyLandscape(landscapeSpanCount: Int): FishBunCreator
+
+    fun setAlbumSpanCountOnlPortrait(portraitSpanCount: Int): FishBunCreator
+
+    fun setAllViewTitle(allViewTitle: String?): FishBunCreator
+
+    fun setActionBarTitle(actionBarTitle: String?): FishBunCreator
+
+    fun setHomeAsUpIndicatorDrawable(icon: Drawable?): FishBunCreator
+
+    fun setDoneButtonDrawable(icon: Drawable?): FishBunCreator
+
+    fun setAllDoneButtonDrawable(icon: Drawable?): FishBunCreator
+
+    fun setIsUseAllDoneButton(isUse: Boolean): FishBunCreator
+
+    fun setMenuDoneText(text: String?): FishBunCreator
+
+    fun setMenuAllDoneText(text: String?): FishBunCreator
+
+    fun setMenuTextColor(color: Int): FishBunCreator
+
+    fun setIsUseDetailView(isUse: Boolean): FishBunCreator
+
+    fun setIsShowCount(isShow: Boolean): FishBunCreator
+
+    fun setSelectCircleStrokeColor(strokeColor: Int): FishBunCreator
+
+    fun isStartInAllView(isStartInAllView: Boolean): FishBunCreator
+}
