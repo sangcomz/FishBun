f4d8b6ff188d06e6b8202703d352bb8d51fa87f0
bhlee <lbsequ@users.noreply.github.com>
2019-09-27 21:32:24 +0900

    Revert "Resolves FishBun#116. Refer to the changes."
    This reverts commit 174c22f404f11cd87d12cfc8e0f15ec8e9e18686.

--- FishBun/src/main/java/com/sangcomz/fishbun/permission/PermissionCheck.java
+++ FishBun/src/main/java/com/sangcomz/fishbun/permission/PermissionCheck.java
@@ -20,12 +20,6 @@ import com.sangcomz.fishbun.define.Define;
 public class PermissionCheck {
     private Context context;
 
-    private final String[] permissions = new String[] {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.CAMERA
-    };
-
     public PermissionCheck(Context context) {
         this.context = context;
     }
@@ -33,16 +27,25 @@ public class PermissionCheck {
 
     @TargetApi(Build.VERSION_CODES.M)
     public boolean CheckStoragePermission() {
-        if (isNecessaryToCheckPermissions()) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    (Activity) context, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                // Show an explanation to the user *asynchronously* -- don't block
+        Define define = new Define();
+        int permissionCheckRead = ContextCompat.checkSelfPermission(context,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+        int permissionCheckWrite = ContextCompat.checkSelfPermission(context,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permissionCheckRead != PackageManager.PERMISSION_GRANTED || permissionCheckWrite != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                // Show an expanation to the user *asynchronously* -- don't block
                 // this thread waiting for the user's response! After the user
                 // sees the explanation, try again to request the permission.
-                requestPermissions();
+                ActivityCompat.requestPermissions((Activity) context,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        define.PERMISSION_STORAGE);
             } else {
                 // No explanation needed, we can request the permission.
-                requestPermissions();
+                ActivityCompat.requestPermissions((Activity) context,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        define.PERMISSION_STORAGE);
 
                 // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
                 // app-defined int constant. The callback method gets the
@@ -53,20 +56,6 @@ public class PermissionCheck {
             return true;
     }
 
-    private boolean isNecessaryToCheckPermissions() {
-        for(int i = 0; i < permissions.length; i++) {
-            int result = ContextCompat.checkSelfPermission(context, permissions[i]);
-            if(result != PackageManager.PERMISSION_GRANTED) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private void requestPermissions() {
-        final Define define = new Define();
-        ActivityCompat.requestPermissions((Activity) context, permissions, define.PERMISSION_STORAGE);
-    }
 
     public void showPermissionDialog() {
         Toast.makeText(context, R.string.msg_permission, Toast.LENGTH_SHORT).show();
