0958c563e6dc7e48281de338651425d997dc9e72
rapbon <rapbong@gmail.com>
2019-10-01 00:19:46 +0900

    Convert com.sangcomz.fishbun.adapter.view Package to Kotlin

---
+++ FishBun/src/main/java/com/sangcomz/fishbun/adapter/view/DetailViewPagerAdapter.kt
@@ -0,0 +1,67 @@
+package com.sangcomz.fishbun.adapter.view;
+
+import android.net.Uri;
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sangcomz.fishbun.Fishton;
+import com.sangcomz.fishbun.R;
+import com.sangcomz.fishbun.util.TouchImageView;
+import kotlinx.android.synthetic.main.detail_item.view.*
+
+/**
+ * Created by sangcomz on 15/06/2017.
+ */
+
+class DetailViewPagerAdapter(private val inflater: LayoutInflater, private val images: Array<Uri>) : PagerAdapter() {
+    private val fishton = Fishton.getInstance()
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val itemView = inflater.inflate(R.layout.detail_item, container, false)
+        container.addView(itemView)
+
+        fishton.imageAdapter
+                .loadDetailImage(itemView.img_detail_image, images[position])
+
+        return itemView.img_detail_image
+    }
+
+    override fun getCount(): Int = images.size
+
+    override fun destroyItem(container: ViewGroup, position: Int, targetObject: Any) {
+        if (container is ViewPager) {
+            container.removeView(targetObject as ConstraintLayout)
+        }
+    }
+
+    override fun isViewFromObject(view: View, targetObject: Any): Boolean {
+        return view == targetObject
+    }
+}
+
+/*
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        if (container instanceof ViewPager) {
+            container.removeView((ConstraintLayout) object);
+        }
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+}
+*/
