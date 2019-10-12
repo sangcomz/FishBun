23b04ccee90e8c37766ed40d18d40ebdc1dd782b
Rapbong <rapbong@gmail.com>
2019-10-12 10:52:54 +0900

    Implement Instrument test code (#186)
    * Implement instrument test code
    
    1. Add Android Test configuration in the build.gradle
    (mockito-android, androidx.test)
    2. Implement RadioWithTextButtonTest
    3. Add VisibleForTesting annotation for unit test
    
    * Revert CameraUtil
    
    I tried to implement CameraUtilTest.
    But, I can't resolve IO Exception.
    Therefore I revert CameraUtil
    
    * Add Instrument test report configuration
    
    * Remove unnecessary changes

--- FishBun/src/main/java/com/sangcomz/fishbun/util/RadioWithTextButton.java
+++ FishBun/src/main/java/com/sangcomz/fishbun/util/RadioWithTextButton.java
@@ -7,6 +7,8 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
+
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
@@ -51,6 +53,13 @@ public class RadioWithTextButton extends View {
         mTextPaint.setFakeBoldText(true);
     }
 
+    @VisibleForTesting(otherwise = VisibleForTesting.NONE)
+    RadioWithTextButton(Context context, Paint textPaint, Paint strokePaint, Paint circlePaint) {
+        super(context);
+        mTextPaint = textPaint;
+        mStrokePaint = strokePaint;
+        mCirclePaint = circlePaint;
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -129,7 +138,8 @@ public class RadioWithTextButton extends View {
      * @param paint        the Paint to set the text size for
      * @param desiredWidth the desired width
      */
-    private static void setTextSizeForWidth(Paint paint, String text, float desiredWidth) {
+    @VisibleForTesting
+    static void setTextSizeForWidth(Paint paint, String text, float desiredWidth) {
 
         // Pick a reasonably large value for the test. Larger values produce
         // more accurate results, but may cause problems with hardware
