43fdabdde655d3f4c561760005cfe4c09ba62104
sangcomz <sangcomz@naver.com>
2019-11-23 00:41:54 +0900

    Fix glide requestOptions 🔧

--- FishBun/src/main/java/com/sangcomz/fishbun/adapter/image/impl/GlideAdapter.kt
+++ FishBun/src/main/java/com/sangcomz/fishbun/adapter/image/impl/GlideAdapter.kt
@@ -2,8 +2,8 @@ package com.sangcomz.fishbun.adapter.image.impl
 
 import android.net.Uri
 import android.widget.ImageView
-
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.request.RequestOptions
 import com.sangcomz.fishbun.adapter.image.ImageAdapter
 
@@ -13,20 +13,25 @@ import com.sangcomz.fishbun.adapter.image.ImageAdapter
 
 class GlideAdapter : ImageAdapter {
     override fun loadImage(target: ImageView, loadUrl: Uri) {
-        val options = RequestOptions().centerCrop()
+        val options = RequestOptions().apply {
+            centerCrop()
+            format(DecodeFormat.PREFER_RGB_565)
+        }
         Glide
-                .with(target.context)
-                .load(loadUrl)
-                .apply(options)
-                .into(target)
+            .with(target.context)
+            .load(loadUrl)
+            .apply(options)
+            .override(target.width, target.height)
+            .thumbnail(0.25f)
+            .into(target)
     }
 
     override fun loadDetailImage(target: ImageView, loadUrl: Uri) {
         val options = RequestOptions().centerInside()
         Glide
-                .with(target.context)
-                .load(loadUrl)
-                .apply(options)
-                .into(target)
+            .with(target.context)
+            .load(loadUrl)
+            .apply(options)
+            .into(target)
     }
 }
